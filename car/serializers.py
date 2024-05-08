eb54baf196312033c7e747d1b1c869b4da1df30b
vital <vitaliyburkalo81@gmail.com>
2024-05-08 13:07:44 +0300

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,42 @@
 from rest_framework import serializers
+from car.models import Car
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from io import BytesIO
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(min_value=20, max_value=100)
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(allow_null=True, required=False)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get(
+            "problem_description", instance.problem_description
+        )
+        instance.save()
+        return instance
+
+
+def serialize_car_object(car):
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
+
+
+def deserialize_car_object(car_json):
+    stream = BytesIO(car_json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return serializer.save()
+    else:
+        raise Exception("Invalid data: " + str(serializer.errors))
